0f2deec304cb09ae88ae84aa94690c0f83caf20b
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-07-09 14:27:41 +0900

    work TransformTween

---
+++ Assets/Scripts/StageEditor/AkaneTransfromTween/TransformTweenTrack.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace AkaneTools.Timeline
+{
+    [TrackColor(0.458f, 0.568f, 0.470f)]
+    [TrackClipType(typeof(Transform))]
+    public class TransformTweenTrack : PlayableTrack
+    {
+
+    }
+}
